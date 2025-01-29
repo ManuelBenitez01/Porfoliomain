e23cd93617266eebd4926b8dfe8cb3efdc3e59f6
ManuelBenitez01 <josemanuelbenitez01@gmail.com>
2025-01-29 11:10:28 -0300

    agregando cosas

---
+++ function.js
@@ -0,0 +1,14 @@
+
+let scroll1 = document.getElementById("scroll");
+if (scroll1) { 
+    window.addEventListener("scroll", () => {
+        let scrollPosition = window.scrollY;
+        if (scrollPosition > 100) {
+            scroll1.classList.add  ("hide");
+        } else if (scrollPosition < 1000) {
+            scroll1.classList.remove ("hide");
+        }
+    });
+} else {
+    console.error("Elemento con id 'scroll' no encontrado.");
+}
